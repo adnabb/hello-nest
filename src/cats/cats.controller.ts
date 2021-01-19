7552634ffe3ef838f4d1509eaa69bdc9b78b46eb
vivi <chen.shi.yu@qq.com>
2021-01-19 22:45:55 +0800

    init

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Header, HttpCode, Post } from '@nestjs/common';
+import { Request } from 'express'
+
+@Controller('cats')
+export class CatsController {
+  @Post()
+  @HttpCode(204)
+  @Header('Cache-Control', 'none')
+  create(): string {
+    return 'This action adds a new cat'
+  }
+
+  @Get()
+  findAll(): string {
+    return 'This action returns all cats'
+  }
+}
